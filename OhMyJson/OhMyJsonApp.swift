6ee954dd246af832227f6570573434752277c48a
JihoonKim <kimzhoon@naverz-corp.com>
2026-02-05 14:00:07 +0900

    Initial Commit

---
+++ OhMyJson/OhMyJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhMyJsonApp.swift
+//  OhMyJson
+//
+//  Created by USER on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct OhMyJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OhMyJsonTests/OhMyJsonTests.swift
@@ -0,0 +1,16 @@
+//
+//  OhMyJsonTests.swift
+//  OhMyJsonTests
+//
+//  Created by USER on 2/5/26.
+//
+
+import Testing
+
+struct OhMyJsonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
